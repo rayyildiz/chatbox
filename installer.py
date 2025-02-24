a50a78866b341d66fd54df51027cc8dcc874ca67
Ramazan A. <r@ayyildiz.ai>
2025-02-24 12:53:10 +0300

    Refactor project structure and update dependencies.
    Migrated core files to the `src` directory for better organization and adjusted paths accordingly. Added `cffi` and `pywin32-ctypes` as new dependencies while refining the `installer.py` script for streamlined builds. Removed unused code, optimized configurations,

--- installer.py
+++ installer.py
@@ -3,14 +3,19 @@ import PyInstaller.__main__
 
 PyInstaller.__main__.run(
     [
-        "app.py",
+        "./src/app.py",
         "--onefile",
         "--windowed",
+        "--clean",
+        "--noconfirm",
+        "--name=chatbox",
         "--icon=./app_icon.ico",
         "--add-data=./app_icon.ico:.",
         "--collect-data=gradio",
         "--collect-data=gradio_client",
-        "--additional-hooks-dir=./hooks",
-        "--runtime-hook=./runtime_hook.py",
+        "--additional-hooks-dir=./src/hooks",
+        "--runtime-hook=./src/runtime_hook.py",
+        # "--exclude-module=cffi",
+        "--exclude-module=.git"
     ]
 )
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from local-chatbox!")
-
-
-if __name__ == "__main__":
-    main()
